816b06e9908c2c2da6d6f8aedec90f44f753994c
TheSwagMan <theswagman@gmx.fr>
2016-12-01 22:54:55 +0100

    Update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ This file contains scripts to resolve problems on turing.nymphomath.ch. Please d
 TheSwagMan - Thomas POTIER <theswagman@gmx.fr>
 """
 
+
 ####################
 # FUNCTIONS        #
 ####################
@@ -27,6 +28,7 @@ def reduce(m):
                 m[i - 1] -= 1
     return m
 
+
 def syracuse(n):
     r = []
     while n > 1:
@@ -42,6 +44,7 @@ def syracuse(n):
 def rotate(n):
     return int(str(n)[-1] + str(n)[:-1])
 
+
 def syracuse_len(n):
     l = 1
     while n > 1:
@@ -51,6 +54,7 @@ def syracuse_len(n):
         else:
             n = 3 * n + 1
     return l
+
 
 def is_prime(n):
     for i in range(2, int(n ** 0.5) + 1):
@@ -66,6 +70,7 @@ def is_palindrom(n):
         return int(s[:l // 2]) == mirror(s[l // 2 + 1:])
     return int(s[:l // 2]) == mirror(s[l // 2:])
 
+
 def sum_of_nums(n):
     return sum([int(e) for e in str(n)])
 
@@ -75,6 +80,7 @@ def facto(n):
     for i in range(2, n):
         r *= i
     return r * n
+
 
 def mirror(n):
     return int(str(n)[::-1])
@@ -120,82 +126,83 @@ def are_facts_fact(n, k):
             s += i
     return s == k * n
 
+
 ####################
 # MAIN PROG        #
 ####################
 
 
 # 1
-somme=0
+somme = 0
 for i in range(2013):
-    if i%5==0 or i%7==0:
-        somme+=i
-print(1,somme)
+    if i % 5 == 0 or i % 7 == 0:
+        somme += i
+print(1, somme)
 
 # 2
-s=0
-a=0
-b=1
-c=1
-while c<4000000:
-    if c%2==0:
-        s+=c
-    a=b
-    b=c
-    c=a+b
-print(2,s)
+s = 0
+a = 0
+b = 1
+c = 1
+while c < 4000000:
+    if c % 2 == 0:
+        s += c
+    a = b
+    b = c
+    c = a + b
+print(2, s)
 
 # 3
-n=20130101
+n = 20130101
 for i in range(n):
-    if n%((n//2)-i)==0:
-        if is_prime((n//2)-i):
+    if n % ((n // 2) - i) == 0:
+        if is_prime((n // 2) - i):
             break
-print(3,(n//2)-i)
+print(3, (n // 2) - i)
 
 # 4
-palindroms=[]
-for i in range(10**3-1,10**2,-1):
-    for j in range(10**4-1,10**3,-1):
-        if is_palindrom(i*j):
-            palindroms.append(i*j)
-print(4,max(palindroms))
+palindroms = []
+for i in range(10 ** 3 - 1, 10 ** 2, -1):
+    for j in range(10 ** 4 - 1, 10 ** 3, -1):
+        if is_palindrom(i * j):
+            palindroms.append(i * j)
+print(4, max(palindroms))
 
 # 5
-print(5,sum_of_nums(2**2222))
+print(5, sum_of_nums(2 ** 2222))
 
 # 6
-print(6,sum_of_nums(facto(2013)))
+print(6, sum_of_nums(facto(2013)))
 
 # 7
-i=2
-a=1
-while a<23456:
-    i+=1
+i = 2
+a = 1
+while a < 23456:
+    i += 1
     if is_prime(i):
-        a+=1
-print(7,i)
+        a += 1
+print(7, i)
 
 # 10
-s=0
-for i in range(2,10000000):
+s = 0
+for i in range(2, 10000000):
     if is_prime(i):
-        s+=i
-print(10,s)
+        s += i
+print(10, s)
 
 # 11
-n=10000000
+n = 10000000
 for i in range(n):
-    k=n-i
-    if mirror(k)==4*k:
+    k = n - i
+    if mirror(k) == 4 * k:
         break
-print(11,k)
+print(11, k)
 
 # 13
 a = 836
 while True:
     a += 1
-    if is_palindrom(a ** 2) and len(str(a**2))%2==0:
+    if is_palindrom(a ** 2) and len(str(a ** 2)) % 2 == 0:
         break
 print(13, a ** 2)
 
@@ -210,66 +217,66 @@ for i in range(1500000):
 print(14, maxi)
 
 # 19
-sum=0
+sum = 0
 for i in range(2013):
-    if (i%(2*6)==1 or i%(2*6)==2*6-1) and (i%(2*7)==1 or i%(2*7)==2*7-1):
-        sum+=i+1
-print(19,sum)
+    if (i % (2 * 6) == 1 or i % (2 * 6) == 2 * 6 - 1) and (i % (2 * 7) == 1 or i % (2 * 7) == 2 * 7 - 1):
+        sum += i + 1
+print(19, sum)
 
 # 25
-b=0
-c=1
-i=1
-while len(str(c))<2013:
-    i+=1
-    k=b+c
-    b=c
-    c=k
-print(25,i)
+b = 0
+c = 1
+i = 1
+while len(str(c)) < 2013:
+    i += 1
+    k = b + c
+    b = c
+    c = k
+print(25, i)
 
 # 34 ERROR !!!
-s=0
-c=3
-while c<1000000:
-    if c==factorial_sum(c):
-        s+=c
-    c+=1
-print(34,s)
+s = 0
+c = 3
+while c < 1000000:
+    if c == factorial_sum(c):
+        s += c
+    c += 1
+print(34, s)
 
 # 48
-s=0
-for i in range(1,2014):
-    s+=i**i
-print(48,str(s)[:10])
+s = 0
+for i in range(1, 2014):
+    s += i ** i
+print(48, str(s)[:10])
 
 # 67
-print(67,diastern(10000001))
+print(67, diastern(10000001))
 
 # 70
-s=0
-for i in range(10**5,10**6):
-    b=rotate(i)
-    if i!=b and len(str(b))==6:
-        if b%i==0:
-            s+=i
-print(70,s)
+s = 0
+for i in range(10 ** 5, 10 ** 6):
+    b = rotate(i)
+    if i != b and len(str(b)) == 6:
+        if b % i == 0:
+            s += i
+print(70, s)
 
 # 71
-a=568
+a = 568
 while not (has_each_num(str(a) + str(a ** 2), 1) and has_different_nums(str(a) + str(a ** 2))):
-    a+=1
-print(71,a)
+    a += 1
+print(71, a)
 
 # 72
-n=0
-a=1
-while a**2<1000000000:
-    k=str(a**2)
-    if len(k)>=4:
+n = 0
+a = 1
+while a ** 2 < 1000000000:
+    k = str(a ** 2)
+    if len(k) >= 4:
         if k[-1] == k[-2] == k[-3] != k[-4]:
             n += 1
-    a+=1
-print(72,n)
+    a += 1
+print(72, n)
 
 # 73
 a = 9999999
@@ -330,6 +337,5 @@ s = 0
 for i in range(1, 100000):
     if are_facts_fact(i, 3):
         s += i
-        print(i)
         print(i)
 print(126, s)
